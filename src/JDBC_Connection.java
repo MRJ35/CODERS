8f03c4f6705e4fe6f0793ed6dc60a761d3ab1827
MRJ35 <43617890+MRJ35@users.noreply.github.com>
2019-06-21 13:01:17 +0530

    update connection file

--- src/JDBC_Connection.java
+++ src/JDBC_Connection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.io.*;
 import javax.sql.*;
 public class JDBC_Connection {
-    public static String connectionAddress = "jdbc:mysql://remotemysql.com:3306/XIqlNMyym7";
-    public static String userName = "XIqlNMyym7";
-    public static String password = "DCFdSrlJgp";
+    public static String connectionAddress = "";
+    public static String userName = "";
+    public static String password = "";
     public static String connect(){
         return connectionAddress;
     }
